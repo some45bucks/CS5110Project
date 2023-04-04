f0289c486cbc570292dfba673c91edcb64578684
some45bucks <70984053+some45bucks@users.noreply.github.com>
2023-04-03 21:05:47 -0600

    fix

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import argparse
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Run a simulation.')
-    parser.add_argument('-n', type=int, default=5, help='number of nodes')
-    parser.add_argument('-m', type=int, default=5, help='number of edges')
+    parser.add_argument('-n', type=int, default=5, help='consumers')
+    parser.add_argument('-m', type=int, default=5, help='producers')
     parser.add_argument('-p', type=float, default=0.4, help='probability of an edge existing')
     parser.add_argument('-sim', type=str, default='default', help='simulation type')
     args = parser.parse_args()

5decf712929dd93d592130a5150788b80ef6a22a
cnspencer <mail.cnspencer@gmail.com>
2023-04-23 19:17:12 -0600

    Added awareness value to calculation of speculative value

--- consumer.py
+++ consumer.py
@@ -5,32 +5,30 @@ class Consumer:
     curr_funds = 0
     risk_tolerance = 0.0001
     id = 0
-    # TODO: Add true preferences of producers
     
     #hidden values
 
     # No awareness on initialization, can change if we want consumers to start with some pre-knowledge
-    def __init__(self, id, funds, preferences, risk_tolerance, true_prefs):
+    def __init__(self, id, funds, preferences, risk_tolerance):
         self.id = id
         self.curr_funds = funds
         self.prefs = preferences
         self.risk_tolerance = risk_tolerance
         self.neighbors = {}
         self.producers = {}
-        self.prefs = preferences
-        self.true = true_prefs
+        #self.true = true_prefs
 
-    def _speculativeValue(self, genre, campaign_goal, curr_campaign_funds):
+    def _speculativeValue(self, genre, campaign_goal, curr_campaign_funds, awareness):
         percentageDone = curr_campaign_funds/campaign_goal
         genrePrefrence = self.prefs[genre]
 
-        return ((percentageDone+1)/self.risk_tolerance)*genrePrefrence
+        return ((percentageDone+1)/self.risk_tolerance)*genrePrefrence*awareness
 
     # Nice wrapper for _speculativeValue()
     def speculativeValue(self, producer):
-        if not self.prefs.contains(producer):
+        if producer not in self.producers:
             return 0  # No value for projects this consumer is unaware of
-        val = self._speculativeValue(producer.genre, producer.campaign_goal, producer.curr_funds)
+        val = self._speculativeValue(producer.genre, producer.campaign_goal, producer.curr_funds, self.producers[producer])
         return val
 
     def modifyPreference(self, genre, modifier):
@@ -43,8 +41,8 @@ class Consumer:
             # significantly greater (>60%) than the total previous contributions
             if contrib <= self.curr_funds:
                 # Ensure we have enough funds to contribute fully
-                producer.addContribution(contrib)
-                self.curr_funds -= contrib
+                producer.addContribution(self.speculativeValue(producer) - producer.getContributions(self))
+                self.curr_funds -= self.speculativeValue(producer) - producer.getContributions(self)
         
     def getId(self):
         return f"Consumer_{self.id}"
